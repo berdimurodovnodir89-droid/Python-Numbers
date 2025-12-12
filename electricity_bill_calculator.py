c2734c4db01f7aa3926ea32fb568bac910b15a1f
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-13 00:48:36 +0500

    bajarildi

--- electricity_bill_calculator.py
+++ electricity_bill_calculator.py
@@ -1,1 +1,11 @@
+from num2words import num2words
+masofa = float(input('masofa :'))
+kelishi = 3.0
+km = 1.20
 
+result = (masofa * km) + km
+words_en = num2words(result, lang='en',to='currency')
+words_ru = num2words(result, lang='ru',to='currency')
+
+print(f'Taxi narxi :{result}')
+print(words_en,words_ru)
