fe1b16028a023d4cb4943276866689f04772ddd6
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-13 01:04:21 +0500

    yangilandi

--- average_spending_tracker.py
+++ average_spending_tracker.py
@@ -1,24 +1,15 @@
 from num2words import num2words
-money = int(input('Summa :'))
-money_copy = money
-money_50=money // 50
-money=money - money_50 * 50
 
-money_10=money // 10
-money=money - money_10 * 10
+monday = float(input('summa :'))
+tuesday = float(input('summa :'))
+wednesday = float(input('summa :'))
+thursday = float(input('summa :'))
+friday= float(input('summa :'))
+saturday = float(input('summa :'))
+sunday = float(input('summa :'))
 
-money_5=money // 5
-money=money - money_5 * 5
-
-money_1=money // 1
-money=money - money_1 * 1
-
-
-result = f'$50 kupyuradan :{money_50} ta \n$10 kupyuradan :{money_10}\n'
-result1 = f'5$ kupyuradan {money_5}ta \n1$ kupyuradan {money_1} ta \n'
-result3 = f'umumiy summa {money_copy} '
-
-words_en = num2words(money_copy, lang='en', to='currency')
-words_ru = num2words(money_copy, lang='ru', to='currency')
-
-print(result, result1, result3,words_en,words_ru)
+total = ( monday + tuesday + wednesday + thursday + friday + saturday + sunday )/7
+words_en = num2words(total,lang='en',to='currency')
+words_ru = num2words(total,lang='ru',to='currency')
+print(f'Ortacha xarajat :{total}')
+print(words_en,words_ru)
