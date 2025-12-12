03dd7f734b9d67c5a9870cfbcea6afa13e7f3fe3
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-13 00:23:49 +0500

    bajarildi

--- cash_denomination_splitter.py
+++ cash_denomination_splitter.py
@@ -1,1 +1,12 @@
+from num2words import num2words
 
+first_price = float(input('First_price :'))
+second_price = float(input('Second_price :'))
+third_price = float(input('Third_price :'))
+
+result = first_price + second_price + third_price 
+
+total_words = num2words(result, lang='en', to='currency')
+
+print(f'umumiy summa {result}')
+print(f'{total_words}')
