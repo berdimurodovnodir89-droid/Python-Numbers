f5c32ada20bdf7ad2287e2a7882bab4f879ceb05
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-13 01:10:46 +0500

    yangilandi

--- delivery_service_price.py
+++ delivery_service_price.py
@@ -1,15 +1,12 @@
 from num2words import num2words
 
-monday = float(input('summa :'))
-tuesday = float(input('summa :'))
-wednesday = float(input('summa :'))
-thursday = float(input('summa :'))
-friday= float(input('summa :'))
-saturday = float(input('summa :'))
-sunday = float(input('summa :'))
+masofa = float(input('masofa :'))
+kelishi = 5.00
+km = 0.80
 
-total = ( monday + tuesday + wednesday + thursday + friday + saturday + sunday )/7
-words_en = num2words(total,lang='en',to='currency')
-words_ru = num2words(total,lang='ru',to='currency')
-print(f'Ortacha xarajat :{total}')
+result = (masofa * km) + kelishi
+words_en = num2words(result, lang='en',to='currency')
+words_ru = num2words(result, lang='ru',to='currency')
+
+print(f'Taxi narxi :{result}')
 print(words_en,words_ru)
