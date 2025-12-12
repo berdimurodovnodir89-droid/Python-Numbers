4143f4cba46cc268e5454811114f7a20ec6fc339
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-13 01:05:18 +0500

    yangilandi

--- cash_denomination_splitter.py
+++ cash_denomination_splitter.py
@@ -1,12 +1,24 @@
 from num2words import num2words
+money = int(input('Summa :'))
+money_copy = money
+money_50=money // 50
+money=money - money_50 * 50
 
-first_price = float(input('First_price :'))
-second_price = float(input('Second_price :'))
-third_price = float(input('Third_price :'))
+money_10=money // 10
+money=money - money_10 * 10
 
-result = first_price + second_price + third_price 
+money_5=money // 5
+money=money - money_5 * 5
 
-total_words = num2words(result, lang='en', to='currency')
+money_1=money // 1
+money=money - money_1 * 1
 
-print(f'umumiy summa {result}')
-print(f'{total_words}')
+
+result = f'$50 kupyuradan :{money_50} ta \n$10 kupyuradan :{money_10}\n'
+result1 = f'5$ kupyuradan {money_5}ta \n1$ kupyuradan {money_1} ta \n'
+result3 = f'umumiy summa {money_copy} '
+
+words_en = num2words(money_copy, lang='en', to='currency')
+words_ru = num2words(money_copy, lang='ru', to='currency')
+
+print(result, result1, result3,words_en,words_ru)
